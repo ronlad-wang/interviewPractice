b512b34a8eb90f64e1486f782431aed63f6246c0
Ronald Wang <ronaldwang2025@berkeley.edu>
2023-01-03 17:13:09 -0800

    implemented optimizations to kthToLast

--- InterviewPractice/src/Chapter2/KthToLast.java
+++ InterviewPractice/src/Chapter2/KthToLast.java
@@ -24,12 +24,40 @@ public class KthToLast {
 
         //once we know the whole size, we can figure out what place from the front k from the back is
         for(int i = 1; i < size - k; i++) {
+            //this question is also slightly unclear, do we get the last value if we ask for the
+            //0th from the end or the 1st from the end? Make sure to clarify this.
+            //this solution assumes that 1st from the end means the last value.
             counter = counter.next;
         }
 
         return counter.item;
 
         //runtime here is O(N) since we have to iterate to the end of the list.
+    }
+
+    public static Object kthToLastSlightOptimization(int k, LinkedList<Integer> l) {
+
+        //this solution uses the two runner system to solve the question in a slightly more
+        //optimal fashion. It is still O(N) time, but now removes the need to perform a second
+        //iteration through the list.
+
+
+        Node fastRunner = l.firstNode.next;
+        Node slowRunner = l.firstNode.next;
+
+
+        for(int i = 0; i < k; i++) {
+            fastRunner = fastRunner.next;
+        }
+        //realize that since fastrunner is now k nodes ahead of slowrunner, it will reach the end
+        //at the same time that slowrunner reaches the kth node from the end.
+
+        while(fastRunner != null) {
+            fastRunner = fastRunner.next;
+            slowRunner = slowRunner.next;
+        }
+
+        return slowRunner.item;
     }
 
     public static void main(String[] args) {
@@ -40,6 +68,6 @@ public class KthToLast {
         test.printList();
 
         System.out.println();
-        System.out.print(kthToLast(4, test));
+        System.out.print(kthToLastSlightOptimization(3, test));
     }
 }
