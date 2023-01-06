d02e142f66522aa48f716d79ed3df81626878755
Ronald Wang <ronaldwang2025@berkeley.edu>
2023-01-06 15:09:10 -0800

    added documentation to SortStack

--- InterviewPractice/src/Chapter3/SortStack.java
+++ InterviewPractice/src/Chapter3/SortStack.java
@@ -11,12 +11,26 @@ public class SortStack {
 
     public static void sortStack(Stack<Integer> s) {
         Stack<Integer> reverseSortedStack = new Stack<>();
+
+        //we're sorting items into the other stack by holding the top item as a variable,
+        //then taking items out of the other stack and into the original stack as long as those
+        //items are larger than the held item. Then we place the item into the other stack, and
+        //replace those items removed back into the other stack (this keeps them in their sorted order)
+        //we repeat this process until the original stack is empty, at which point the
+        //other stack is sorted in the opposite order.
+
+        //finally, taking each element out of the other stack and placing it in the original stack
+        //makes the original stack sorted.
+
         int numPopped = 0;
         while(!s.isEmpty()) {
             int topItem = s.pop();
             while(!reverseSortedStack.isEmpty() && topItem < reverseSortedStack.peek()) {
                 s.push(reverseSortedStack.pop());
                 numPopped++;
+                //we count the number of times we moved items from the other stack to the original
+                //stack so that we know how many we need to replace after the item goes into the
+                //correct spot.
             }
             reverseSortedStack.push(topItem);
             for(int i = 0; i < numPopped; i++) {
@@ -28,6 +42,8 @@ public class SortStack {
         while(!reverseSortedStack.isEmpty()) {
             s.push(reverseSortedStack.pop());
         }
+
+        //runtime is O(N^2) in case we're given a stack that is in the opposite sorted order.
     }
 
     public static void main(String[] args) {
