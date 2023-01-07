9bd989c06e1f9fc2b9252989b0000f8720e123b1
Ronald Wang <ronaldwang2025@berkeley.edu>
2023-01-06 21:37:49 -0800

    implemented graph nodes for chapter 4

---
+++ InterviewPractice/src/DataStructures/GraphNode.java
@@ -0,0 +1,21 @@
+package DataStructures;
+
+import java.util.ArrayList;
+
+public class GraphNode {
+    private int item;
+    private ArrayList<GraphNode> connections;
+
+    public GraphNode(int i) {
+        item = i;
+    }
+    public void connect(GraphNode to) {
+        connections.add(to);
+    }
+    public int getItem() {
+        return item;
+    }
+    public ArrayList<GraphNode> getConnections() {
+        return connections;
+    }
+}
