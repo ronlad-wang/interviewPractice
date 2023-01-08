2a28b84abd1cd894a301391ef35923b8cdede47a
Ronald Wang <ronaldwang2025@berkeley.edu>
2023-01-07 19:21:37 -0800

    implemented optimzations for ValidateBST

--- InterviewPractice/src/Chapter4/ValidateBST.java
+++ InterviewPractice/src/Chapter4/ValidateBST.java
@@ -7,23 +7,23 @@ public class ValidateBST {
     Implement a function to check if a binary tree is a binary search tree.
      */
 
-    public static boolean isBST(BinaryNode root) {
+    public static boolean isBST(BinaryNode root, Integer min, Integer max) {
+        //we should check first to understand the properties of how duplicate values are handled
+
         //we can take advantage of the fact that if a node is the root of a binary tree, either its
-        //subnodes are all BSTs or they are null.
+        //subnodes are all BSTs or they are null. The two criteria are then, if the subnodes are
+        //BSTs, and if the root node of those subtrees are correct compared to our current item
 
-        if(root.left != null && (!isBST(root.left) || root.left.item > root.item)) {
-            //if the left isn't a BST, then we have a problem. If the left's root is greater than our
-            //root item, then we have a problem
+        if(root == null) {
+            return true;
+        }
+        if((min != null && root.item <= min) || (max != null && root.item > max)) {
             return false;
         }
-        if(root.right != null && (!isBST(root.right) || root.right.item < root.item)) {
-            //vice versa here
+        if(!isBST(root.left, min, root.item) || !isBST(root.right, root.item, max)) {
             return false;
         }
-
         return true;
-
-
     }
 
 }
